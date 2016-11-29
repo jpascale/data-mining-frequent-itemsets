4ba70e84f62e307589ab5e39f1d87f40b83a712d
jpascale <jpascale@itba.edu.ar>
2016-11-29 09:05:04 +0100

    coded, not tested

--- itemsets.py
+++ itemsets.py
@@ -1,10 +1,14 @@
+import itertools
+
 class ItemSet(object):
 
-	def __init__(self, filename, s):
+	def __init__(self, filename, s, k):
 
 		#Do the initial count (Compulsory for every k, s)
 		self.dict = dict()
 		self.s = s
+		self.k = k
+		self.filename = filename
 
 		with open(filename, 'r') as fd:
 			while True:
@@ -12,14 +16,13 @@ class ItemSet(object):
 					break
 
 				line = fd.readline()
-				self.initial_line_process(line)
+				self.__initial_line_process__(line)
 
-			print len(self.dict)
-			self.apply_threshold()
-			print len(self.dict)
+			self.__apply_initial_threshold__()
+			self.k_dict = self.dict
 
 
-	def initial_line_process(self, line):
+	def __initial_line_process__(self, line):
 		transaction = line.strip().split()
 
 		for item in transaction:
@@ -28,10 +31,69 @@ class ItemSet(object):
 			else:
 				self.dict[item] = 1
 
-	def apply_threshold(self):
+	def __apply_initial_threshold__(self):
 		#We kill every item that is lower than the threshold
 		for item in self.dict.keys():
 			if not self.dict[item] >= self.s:
 				del(self.dict[item])
 
-	def generate_k_dict(self, k)
+	def __generate_k_empty_dict__(self, k):
+		self.k_dict = dict.fromkeys(set(itertools.combinations(self.dict, k)), 0)
+
+	def __read_k_dict__(self):
+		with open(self.filename, 'r') as fd:
+			while True:
+				
+				line = fd.readline()
+				if line == '':
+						break
+
+				transaction = line.strip().split()
+
+				for key_tuple in self.k_dict.keys():
+					key_in_transaction = True
+					for individual_key in key_tuple:
+						if not individual_key in transaction:
+							key_in_transaction = False
+							break
+
+					if key_in_transaction:
+						self.k_dict[key_tuple] = self.k_dict[key_tuple] + 1
+			print "__read_k_dict__ finished"
+			print self.k_dict.values()
+			while True:
+				pass
+
+	def __apply_k_threshold__(self):
+		#We kill every item that is lower than the threshold
+		for item in self.k_dict.keys():
+			if not self.k_dict[item] >= self.s:
+				del(self.k_dict[item])
+
+	def __update_original_dict__(self):
+		for key in self.dict:
+			found = False
+			for tuple_key in self.k_dict:
+				for k_key in tuple_key:
+					if key == k_key:
+						found = True
+						break
+			if not found:
+				del(self.dict[key])
+
+
+	def get_k_dict(self):
+		k = self.k
+
+		if k < 1:
+			return None
+		if k == 1:
+			return self.dict
+
+		for i in range(k + 1)[2:]: #Only possible to make it from 2 and on
+			self.__generate_k_empty_dict__(i)
+			self.__read_k_dict__()
+			self.__apply_k_threshold__()
+			self.__update_original_dict__(self)
+
+		return self.k_dict
