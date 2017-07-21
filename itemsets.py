27ee057ff2c782fe9189577ea39fddc4e7b8c0ac
jpascale <jpascale@itba.edu.ar>
2017-07-21 04:25:57 -0300

    Calculate implications

--- itemsets.py
+++ itemsets.py
@@ -1,6 +1,29 @@
 import itertools
 import time
 
+def difference(a, b):
+	return tuple(x for x in a if x not in b)
+
+def sort_tuple(tup):
+	if len(tup) == 1:
+		return tup[0]
+
+	asd = [int(x) for x in tup]
+	asd.sort()
+	return tuple([str(x) for x in asd])
+
+class Rule(object):
+	def __init__(self, I, j):
+		self.I = I
+		self.j = j
+
+	def __repr__(self):
+		return str(self.I) + " -> " + str(self.j)
+
+	def set_params(self, interest):
+		self.interest = interest
+
+
 class ItemSet(object):
 
 	def __init__(self, filename, s, k):
@@ -10,6 +33,9 @@ class ItemSet(object):
 		self.s = s
 		self.k = k
 		self.filename = filename
+
+		#Every i element corresponsds to K = I + 1
+		self.bkp_k_dict = []
 
 		with open(filename, 'r') as fd:
 			while True:
@@ -21,6 +47,9 @@ class ItemSet(object):
 
 			self.__apply_initial_threshold__()
 			self.k_dict = self.dict
+
+			#Save dict for K = 1
+			self.bkp_k_dict.append(self.dict.copy())
 
 
 	def __initial_line_process__(self, line):
@@ -40,31 +69,6 @@ class ItemSet(object):
 
 	def __generate_k_empty_dict__(self, k):
 		self.k_dict = dict.fromkeys(set(itertools.combinations(self.dict, k)), 0)
-	'''
-	#try to optimize
-	def __OLD_read_k_dict__(self):
-		print "llegue hasta aca"
-		import time
-		start_time = time.time()
-		with open(self.filename, 'r') as fd:
-			index = 1
-			while True:
-				print "iteration " + str(index)
-				line = fd.readline()
-				if line == '':
-						break
-
-				transaction = set(line.strip().split())
-				
-				for key_tuple in self.k_dict.keys():
-					if transaction.issuperset(key_tuple):
-						self.k_dict[key_tuple] = self.k_dict[key_tuple] + 1
-
-				index += 1
-			print "__read_k_dict__ finished"
-			print self.k_dict.values()
-			print("--- %s seconds ---" % (time.time() - start_time))
-	'''
 
 	##new version
 	def __read_k_dict__(self, curr_k):
@@ -115,9 +119,49 @@ class ItemSet(object):
 			print "DEBUG> Updating original dict (k = " + str(i) + ")"
 			self.__update_original_dict__()
 			
+			print "DEBUG> Generating bkp_k_dict (k = " + str(i) + ")"
+			self.bkp_k_dict.append(self.k_dict.copy())
+
 			print "Done\n"
 
 		return self.k_dict
+
+	def print_rules(self):
+
+		keys = self.k_dict.copy().keys()
+		rules = []
+
+		bkp_k_merged_dict = dict()
+		for a in range(len(self.bkp_k_dict)):
+			bkp_k_merged_dict.update(self.bkp_k_dict[a])
+
+		#For every subset generate I -> j rule
+		for k in range(self.k)[1:]:
+			for key in keys:
+				combinations = set(itertools.combinations(key, k))
+				for combination in combinations:
+					rules.append(Rule(difference(key, combination), combination))
+
+		print "DEBUG> Rules generated"
+		print rules
+
+		print "DEBUG> Calculating confidence and interest for each rule"
+		#import ipdb; ipdb.set_trace()
+
+		#CONVERT TO SORT - THIS IS HORRIBLE
+
+
+		#Iterate over rules to check confidence and interest
+		for rule in rules:
+			print str(rule.I) + " -> " + str(rule.j)
+			#import ipdb; ipdb.set_trace()
+			interest = bkp_k_merged_dict[sort_tuple(tuple(rule.I) + tuple(rule.j))] / float(bkp_k_merged_dict[sort_tuple(tuple(rule.I))])
+			rule.set_params(interest)
+			print str(rule.I) + " -> " + str(rule.j) + " - [" + str(rule.interest * 100) + "%]"
+
+		#import ipdb; ipdb.set_trace()
+
+		
 
 
 if __name__ == '__main__':
@@ -127,10 +171,11 @@ if __name__ == '__main__':
 	a = ItemSet("data.dat", 600, 3)
 	a_dict = a.get_k_dict()
 	print("Time to perform operation: %s seconds" %  (time.time() - t))
-
+	
 	for k, v in a_dict.iteritems():
 		print str(k) + ": with support " + str(v)
 	
+	a.print_rules()
  	print "________________________________"
 
 	print "Test2: threshold s = 1000 and k = 2"
@@ -141,7 +186,8 @@ if __name__ == '__main__':
 	
 	for k, v in a_dict.iteritems():
 		print str(k) + ": with support " + str(v)
-		
+	
+	a.print_rules()	
 	print "________________________________"
 
 	print "Test3: threshold s = 3000 and k = 1"
@@ -149,9 +195,10 @@ if __name__ == '__main__':
 	a = ItemSet("data.dat", 3000, 1)
 	a_dict = a.get_k_dict()
 	print("Time to perform operation: %s seconds" %  (time.time() - t))
-
+	
 	for k, v in a_dict.iteritems():
 		print str(k) + ": with support " + str(v)
+	a.print_rules()
 
 
 
